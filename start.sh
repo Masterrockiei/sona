bacec0e8747c068df68a3aea99f3bd554e78a49e
Amal nath <96761219+Masterrockiei@users.noreply.github.com>
2022-07-11 17:38:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Masterrockiei/Sona.git /Sona
+  git clone https://github.com/Masterrockiei/sona.git /sona
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Sona
+  git clone $UPSTREAM_REPO /sona
 fi
-cd /Sona
+cd /sona
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
