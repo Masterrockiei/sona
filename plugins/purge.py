8649f3b805972a16969578f8889343099473ff37
Amal nath <96761219+Masterrockiei@users.noreply.github.com>
2022-07-20 12:33:20 +0530

    Update purge.py

--- plugins/purge.py
+++ plugins/purge.py
@@ -5,7 +5,7 @@ import asyncio
 from pyrogram import Client, filters
 
 from plugins.admin_check import admin_check
-from plugins.helper_functions.cust_p_filters import f_onw_fliter
+from plugins.cust_p_filters import f_onw_fliter
 
 
 @Client.on_message(
